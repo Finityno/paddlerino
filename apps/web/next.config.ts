9e00dcd1369e85b9c27f2093af6d26a5cbc6e93c
Sondre <68125004+Finityno@users.noreply.github.com>
2026-02-26 14:13:54 +0100

    initial commit

---
+++ apps/web/next.config.ts
@@ -0,0 +1,9 @@
+import "@paddlerino/env/web";
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  typedRoutes: true,
+  reactCompiler: true,
+};
+
+export default nextConfig;
